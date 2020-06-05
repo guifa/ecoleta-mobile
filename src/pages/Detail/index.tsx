8d4b5b89f76e41c3a275e9edbb2f0d8395bc5c0b
Luiz Guilherme Ferreira de Alcântara <luizgfa@gmail.com>
2020-06-05 02:53:05 -0300

    Add business to detail

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Constants from 'expo-constants';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native'
-import { View, StyleSheet, TouchableOpacity, Text, SafeAreaView, Image } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { View, StyleSheet, TouchableOpacity, Text, Image, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
-import MapView, { Marker } from 'react-native-maps';
-import { SvgUri } from 'react-native-svg'
+import api from '../../services/api';
+import * as MailComposer from 'expo-mail-composer';
+
+interface RouteParams {
+    point_id: number
+};
+
+interface Data {
+    point: {
+        name: string,
+        image: string,
+        email: string,
+        whatsapp: number,
+        city: string,
+        uf: string
+    },
+    items: {
+        title: string
+    }[]
+    
+};
 
 const Detail = () => {
     const navigation = useNavigation();
+    const route = useRoute();
+
+    const routeParams = route.params as RouteParams;
+
+    const [data, setData] = useState<Data>({} as Data);
+
+    useEffect(() => {
+        api.get(`points/${routeParams.point_id}`).then(response => {
+            setData(response.data);
+        })
+      }, []);
 
     function handleNavigateBack() {
         navigation.goBack();
+    }
+    
+    function handleWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}`);
+    }
+    
+    function handleComposeMail() {
+        MailComposer.composeAsync({
+            subject: 'Ecoleta - Coleta de Resíduos',
+            recipients: [data.point.email]
+        });
+    }
+
+    if (!data.point) {
+        return null;
     }
 
     return (
@@ -21,21 +66,21 @@ const Detail = () => {
                     <Icon name="arrow-left" size={20} color="#34cb79"></Icon>
                 </TouchableOpacity>
             
-                <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }}></Image>
-                <Text style={styles.pointName}>Teste</Text>
-                <Text style={styles.pointItems}>Lâmpadas</Text>
+                <Image style={styles.pointImage} source={{ uri: data.point.image }}></Image>
+                <Text style={styles.pointName}>{data.point.name}</Text>
+                <Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
 
                 <View style={styles.address}>
                     <Text style={styles.addressTitle}>Endereço</Text>
-                    <Text style={styles.addressContent}>Fortaleza, CE</Text>
+                    <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
                 </View>
             </View>
             <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleWhatsapp}>
                     <FontAwesome name="whatsapp" size={20} color="#FFF"></FontAwesome>
                     <Text style={styles.buttonText}>Whatsapp</Text>
                 </RectButton>
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleComposeMail}>
                     <Icon name="mail" size={20} color="#FFF"></Icon>
                     <Text style={styles.buttonText}>E-mail</Text>
                 </RectButton>
