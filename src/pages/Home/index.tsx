64ac30797936ddc3d3c52d5ba382443ecd5eef9c
Luiz Guilherme Ferreira de Alcântara <luizgfa@gmail.com>
2020-06-05 02:51:46 -0300

    Create home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import axios from 'axios';
 import { useNavigation } from '@react-navigation/native'
 import { Feather as Icon } from '@expo/vector-icons';
 import { View, Image, StyleSheet, Text, ImageBackground } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import RNPickerSelect from 'react-native-picker-select';
  
+interface IbgeUf {
+  sigla: string
+}
+
+interface IbgeCity {
+  nome: string
+}
+
 const Home = () => {
     const navigation = useNavigation();
 
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+    const [selectedUf, setSelectedUf] = useState('0');
+    const [selectedCity, setSelectedCity] = useState('0');
+
     function handleNavigationToPoints() {
-        navigation.navigate('Points');
+        navigation.navigate('Points', {
+          selectedUf,
+          selectedCity
+        });
     }
+
+    useEffect(() => {
+      axios.get<IbgeUf[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+          const ufsInitials = response.data.map(uf => uf.sigla);
+          
+          setUfs(ufsInitials);
+      })
+  }, []);
+  
+  useEffect(() => {
+      if (selectedUf === '0') {
+          return;
+      }
+      axios.get<IbgeCity[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+          const cities = response.data.map(city => city.nome);
+          
+          setCities(cities);
+      })
+  }, [selectedUf]);
+
+  function handleSelectUf(event: string) {
+      const uf = event;
+
+      setSelectedUf(uf);
+  }
+  
+  function handleSelectCity(event: string) {
+      const city = event;
+
+      setSelectedCity(city);
+  }
 
     return (
         <ImageBackground source={require('../../assets/home-background.png')} style={styles.container}
@@ -21,14 +70,24 @@ const Home = () => {
             </View>
 
             <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={handleNavigationToPoints}>
-                    <View style={styles.buttonIcon}>
-                    <Text>
-                        <Icon name="arrow-right" color="#FFF" size={24}></Icon>
-                    </Text>
-                    </View>
-                    <Text style={styles.buttonText}>Entrar</Text>
-                </RectButton>
+              <RNPickerSelect onValueChange={handleSelectUf} value={selectedUf}
+                placeholder={{ label: 'Selecione uma UF', value: 0 }}
+                items={ufs.map(uf => (
+                  { label: uf, value: uf }
+                ))}/>
+              <RNPickerSelect onValueChange={handleSelectCity} value={selectedCity}
+                placeholder={{ label: 'Selecione uma Cidade', value: 0 }}
+                items={cities.map(city => (
+                  { label: city, value: city }
+                ))}/>
+              <RectButton style={styles.button} onPress={handleNavigationToPoints}>
+                  <View style={styles.buttonIcon}>
+                  <Text>
+                      <Icon name="arrow-right" color="#FFF" size={24}></Icon>
+                  </Text>
+                  </View>
+                  <Text style={styles.buttonText}>Entrar</Text>
+              </RectButton>
             </View>
         </ImageBackground>
     );
